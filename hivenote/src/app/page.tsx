b109e71393717390485892baf888b3413f05f782
Devansh <devanshsoni208@gmail.com>
2026-01-21 22:48:54 +0530

    created next-app,installed prisma

---
+++ hivenote/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold text-yellow-500">HiveNote</h1>
+      <p>Centralized learning resources for students</p>
+    </main>
+  );
+}
